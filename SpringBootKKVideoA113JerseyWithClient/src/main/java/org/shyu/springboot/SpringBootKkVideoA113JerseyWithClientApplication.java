a3aabb32b2cf42c0c2fb43d60c9eef25bf534f74
jackyshyu <jackyshyu@gmail.com>
2018-10-23 10:38:53 -0500

    Create project

---
+++ SpringBootKKVideoA113JerseyWithClient/src/main/java/org/shyu/springboot/SpringBootKkVideoA113JerseyWithClientApplication.java
@@ -0,0 +1,12 @@
+package org.shyu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKkVideoA113JerseyWithClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKkVideoA113JerseyWithClientApplication.class, args);
+	}
+}
